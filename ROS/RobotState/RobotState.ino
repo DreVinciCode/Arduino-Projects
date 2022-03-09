2a602e30c956b62af882c470eed009ef831da80e
DreVinciCode <andre.cleaver@tufts.edu>
2022-03-08 23:40:41 -0500

    Update RobotState.ino

--- ROS/RobotState/RobotState.ino
+++ ROS/RobotState/RobotState.ino
@@ -7,23 +7,14 @@ Tufts University
 
 #include <ros.h>
 #include <std_msgs/ColorRGBA.h>
-#include <std_msgs/String.h>
-#include <std_msgs/Empty.h>
 
 ros::NodeHandle  nh;
-std_msgs::String str_msg;
 
-char hello[13] = "hello world!";
 int red_light_pin = 11;
 int green_light_pin = 10;
 int blue_light_pin = 9;
 
-void messageCb( const std_msgs::Empty& toggle_msg)
-{
-  digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-}
-
-void RGBLEDcolorMessage( const std_msgs::ColorRGBA& color_msg)
+void RGBLEDcolorMessageReceiver( const std_msgs::ColorRGBA& color_msg)
 {
   RGB_color(color_msg.r, color_msg.g, color_msg.b);
 }
@@ -35,9 +26,7 @@ void RGB_color(float red_light_value, float green_light_value, float blue_light_
   analogWrite(blue_light_pin, blue_light_value);
 }
 
-ros::Publisher chatter("chatter", &str_msg);
-ros::Subscriber<std_msgs::Empty> sub("toggle_led", messageCb);
-ros::Subscriber<std_msgs::ColorRGBA> colorSub("/SENSAR/robotstate_color", RGBLEDcolorMessage);
+ros::Subscriber<std_msgs::ColorRGBA> colorSub("/SENSAR/robotstate_color", RGBLEDcolorMessageReceiver);
 
 void setup()
 {
@@ -47,16 +36,11 @@ void setup()
   
   pinMode(13, OUTPUT);
   nh.initNode();
-  nh.advertise(chatter);
-  nh.subscribe(sub);
   nh.subscribe(colorSub);
 }
 
-
 void loop()
 {  
-  str_msg.data = hello;
-  chatter.publish( &str_msg );
   nh.spinOnce();
-  delay(500);
+  delay(50);
 }
